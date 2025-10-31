269e469bd28205d4231f3a3f9267119053503c06
Artem Fedoseev <Artikul563@gmail.com>
2025-10-31 22:41:01 +0300

    Сделал правильную архитектуру

--- app/routes.py
+++ app/routes.py
@@ -1,52 +1,23 @@
 from app import app
 
-
-@app.route('/')
-def home():
-    return 'This is the home page'
-
-
-@app.route('/about')
-def about():
-    return 'About page'
+@app.route("/hello")
+def hello():
+    return "Hello World!"
 
 
-@app.route('/contact')
-def contact():
-    return 'Contact page'
-
-
-@app.route('/greet/<name>')
-def helli_name(name):
-    return f'Hello {name}!'
-
-
-@app.route('/user/<int:id>')
-def user_id(id: int) -> str:
-    return f'User ID : {id}'
-
-
-@app.route('/info')
+@app.route("/info")
 def info():
-    return 'This is the info page'
+    return "This is an information page"
 
 
-@app.route('/calc/<int:num1>/<int:num2>')
-def calc(num1,num2):
-    try:
-        return f'The sum of {num1} and {num2} is {num1+num2}'
-    except ValueError:
-        print("Введите корректное число!")
+@app.route("/reverse/<text>")
+def reverse(text):
+    return text[::-1]
 
-@app.route('/reverse/<message>')
-def reverse_message(message):
-    if len(message) < 2:
-        return 'Слишком короткое сообщение!'
-    return message[0:0-1]
+@app.route("/user/<name>/<int:age>")
+def user(name,age):
+    return f"Hello,{name}, you are {age} years old"
 
-@app.route('/user/<name>/<int:age>')
-def user_info(name,age):
-    if age < 0:
-        return f'Вы ввели неправильный возраст'
-    else:
-        return f'Hello, {name}! You are {age} years old'
+@app.route("/calc/<int:a>/<int:b>")
+def clac(a,b):
+    return str(a+b)
